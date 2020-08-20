cd25c02ef8ba1996e568e2dadb92cca79974dd36
Eddie Imada <eddie.imada@gmail.com>
2020-08-19 22:40:10 -0400

    Update 00prepareData.R
    Make clear origin of TPM

--- GTEX/00prepareData.R
+++ GTEX/00prepareData.R
@@ -7,9 +7,11 @@ library(recount)
 download_study('GTEX', type = 'rse-fc')
 
 ### Scale data
+GTEX_raw <- rse_fc
 GTEX <- scale_counts(rse_fc, by="auc")
 
 ### Extract counts matrix
+rseCounts_raw <- assays(GTEX_raw)$counts
 rseCounts <- assays(GTEX)$counts
 ### Extract feature information
 grList <- rowRanges(GTEX)
@@ -20,9 +22,13 @@ pheno <- as.data.frame(colData(GTEX))
 ### Check number of samples and projects available
 table(pheno$gdc_cases.project.project_id)
 
-### Create expression set
+### Create expressions sets
 gtexEset <- ExpressionSet(assayData = rseCounts,
+                          phenoData = AnnotatedDataFrame(pheno),
+                          featureData= AnnotatedDataFrame(fdata))
+gtexEset_raw <- ExpressionSet(assayData = rseCounts_raw,
                           phenoData = AnnotatedDataFrame(pheno),
                           featureData= AnnotatedDataFrame(fdata))
 
 save(gtexEset, file="gtexEset.rda")
+save(gtexEset, file="gtexEset_raw.rda")
